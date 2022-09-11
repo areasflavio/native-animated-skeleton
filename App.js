b8028194c545a685477f23d12b10d3faa72807cd
Flávio Arêas <vflavio9@gmail.com>
2022-09-10 22:21:04 -0300

    feat: create RN Reanimated example

--- App.js
+++ App.js
@@ -1,19 +1,47 @@
-import MaskedView from '@react-native-masked-view/masked-view';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useCallback, useState } from 'react';
+import { Pressable, StyleSheet, Switch, Text, View } from 'react-native';
+import Reanimated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from 'react-native-reanimated';
 
 export default function App() {
+  const [isSelected, setIsSelected] = useState(false);
+  const myVal = useSharedValue(0);
+
+  const handlePress = useCallback(() => {
+    myVal.value = withTiming(myVal.value + 10);
+  }, [myVal]);
+
+  const circleStyle = useAnimatedStyle(
+    () => ({
+      transform: [
+        {
+          scale: isSelected
+            ? withTiming(2, { duration: 100 })
+            : withTiming(1, { duration: 1000 }),
+        },
+        {
+          translateX: myVal.value,
+        },
+      ],
+    }),
+    [isSelected, myVal]
+  );
+
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
 
-      <MaskedView maskElement={<Text style={styles.text}>Hello World</Text>}>
-        <View style={styles.content}>
-          <View style={styles.first}></View>
-          <View style={styles.second}></View>
-          <View style={styles.third}></View>
-        </View>
-      </MaskedView>
+      <Pressable onPress={handlePress}>
+        <Reanimated.View style={[styles.circle, circleStyle]}></Reanimated.View>
+        <Switch
+          value={isSelected}
+          onValueChange={() => setIsSelected((val) => !val)}
+        />
+      </Pressable>
     </View>
   );
 }
@@ -28,6 +56,13 @@ const styles = StyleSheet.create({
   box: {
     height: 100,
     width: 100,
+  },
+  circle: {
+    height: 100,
+    width: 100,
+    borderRadius: 100,
+    margin: 100,
+    backgroundColor: 'pink',
   },
   text: {
     fontSize: 48,
